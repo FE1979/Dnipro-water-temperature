5186ba532dd7ae94c332fe305eae724b438f80de
Roman <ronin911@gmail.com>
2019-04-12 18:20:46 +0300

    Meteopost returns data in correct format

--- meteopost.py
+++ meteopost.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import time
-import plotly.plotly as py
 
 
 def get_page(date):
@@ -63,14 +62,42 @@ def transform_table(weather_table):
         :return: transformed_weather_table
         :rtype: list
     """
-    pass
+    def get_number(string):
+        """ gets only number from a string """
+        regex = r'\d+'
+        num_match = re.match(regex, item[2])
+        return int(num_match.group())
+
+
+    transformed_weather_table = []
+
+    for item in weather_table:
+        transformed_weather_table.append([])
+        # get time
+        regex = r'\d+:\d\d'
+        time_match = re.match(regex, item[0])
+        transformed_weather_table[-1].append(time_match.group())
+
+        # get air temp
+        air_temp = int(item[1][:-1]) # remove grade sign
+        transformed_weather_table[-1].append(air_temp)
+
+        # get pressure, wind direction, wind speed and humidity
+        for i in range(2,5):
+            transformed_weather_table[-1].append(get_number(item[i]))
+
+        # get weather condition
+        transformed_weather_table[-1].append(str(item[6]))
+
+    return transformed_weather_table
 
 
 page = get_page(time.localtime())
 wt =  get_weather_table(page)
+twt = transform_table(wt)
 # print(wt)
 
-for item in wt:
-    print(wt.index(item), item)
+for item in twt:
+    print(twt.index(item), item)
 
 # print(get_weather_table(page))
